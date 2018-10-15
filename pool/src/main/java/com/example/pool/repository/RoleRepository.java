957ca84dfd8ea086745a0c8a3c3103db63791747
korneliarohulko <korneliarohulko@gmail.com>
2018-10-16 00:23:02 +0200

    Add security classess

---
+++ pool/src/main/java/com/example/pool/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.pool.repository;
+
+import com.example.pool.model.Role;
+import com.example.pool.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
